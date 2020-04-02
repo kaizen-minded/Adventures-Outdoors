b437324c39eb39e6400d77504419a12408b285a9
kaizen-minded <melvin3ezell@gmail.com>
2020-04-01 23:59:33 -0400

    add eventlistener on close and menu icon

---
+++ main.js
@@ -0,0 +1,10 @@
+// Finction expression to select elements
+const selectElement = s => document.querySelector(s);
+//Open the menu on click
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
+});
+
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
+});
