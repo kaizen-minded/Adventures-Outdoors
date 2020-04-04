b385a5f703530539ca0519854e0cd6259025b1ae
kaizen-minded <melvin3ezell@gmail.com>
2020-04-03 23:56:06 -0400

    add comment

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const selectElement = s => document.querySelector(s);
 selectElement(".open").addEventListener("click", () => {
   selectElement(".nav-list").classList.add("active");
 });
-
+// Close the menu on click
 selectElement(".close").addEventListener("click", () => {
   selectElement(".nav-list").classList.remove("active");
 });
